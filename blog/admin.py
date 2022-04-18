fefb699997cb62cb1a7439162a92e130abdc44ae
JakubKocerha <jakubbubble@gmail.com>
2022-04-18 11:24:22 +0000

    Added blog template

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,33 @@
+# code adopted from https://github.com/gomathishankar28/ms4_bubbles/tree/main/blog
 from django.contrib import admin
+from .models import Blog, Post, Comment
 
-# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+    )
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'intro',
+        'article',
+        'image',
+        'date_added',
+        'author'
+    )
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'comment_desc',
+        'date_added',
+    )
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Comment, CommentAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+""" import post model to add/edit and display posts"""
 
-# Create your views here.
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Post
+
+
+def blog(request):
+    """ BLog page to display all available posts """
+
+    posts = Post.objects.all()  # pylint: disable=no-member
+    post_count = len(posts)
+    template = 'blog/blog.html'
+    context = {
+        'posts': posts,
+        'post_count': post_count
+    }
+    return render(request, template, context)
