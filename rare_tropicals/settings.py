108ba4f1ba65d411e50de1164ce54fbd1e5ebdba
JakubKocerha <jakubbubble@gmail.com>
2022-04-19 17:55:38 +0000

    Removed secret key and set debug

--- rare_tropicals/settings.py
+++ rare_tropicals/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cl(na!+obpvxljdaeh3-lyhc&%0+j@024jh%m!7fb+_8l+9i4v'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['rare-tropicals.herokuapp.com', 'localhost']
 
