f09c31b9dc289c05d99964e64ca4b3b35730a37b
JakubKocerha <jakubbubble@gmail.com>
2022-04-21 00:45:33 +0000

    Set sending real emails functionality

--- rare_tropicals/settings.py
+++ rare_tropicals/settings.py
@@ -108,9 +108,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-# logging emails to the console to get the comfirmation links
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 # authentication criterias
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
@@ -221,4 +218,15 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'raretropicals@example.com'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'raretropicals@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
