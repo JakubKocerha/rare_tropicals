4d930ffe20f779f796ee03db44470282be5fc592
JakubKocerha <jakubbubble@gmail.com>
2022-04-22 16:31:49 +0000

    Cleaning checkout app code

--- checkout/views.py
+++ checkout/views.py
@@ -28,8 +28,9 @@ def cache_checkout_data(request):
         })
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(request, 'Sorry, your payment cannot be \
-            processed right now. Please try again later.')
+        messages.error(request, ('Sorry, your payment cannot be '
+                                 'processed right now. Please try '
+                                 'again later.'))
         return HttpResponse(content=e, status=400)
 
 
@@ -67,29 +68,28 @@ def checkout(request):
                             product=product,
                             quantity=item_data,
                         )
-                    order_line_item.save()
+                        order_line_item.save()
                 except Product.DoesNotExist:
                     messages.error(request, (
-                        "One of the products in your bag wasn't \
-                        found in our database. "
+                        "One of the products in your bag wasn't "
+                        "found in our database. "
                         "Please call us for assistance!")
                     )
                     order.delete()
                     return redirect(reverse('view_bag'))
 
+            # Save the info to the user's profile if all is well
             request.session['save_info'] = 'save-info' in request.POST
-            return redirect(
-                reverse('checkout_success', args=[order.order_number])
-            )
+            return redirect(reverse('checkout_success',
+                                    args=[order.order_number]))
         else:
-            messages.error(request, 'There was an error with your form. \
-                Please double check your information.')
+            messages.error(request, ('There was an error with your form. '
+                                     'Please double check your information.'))
     else:
         bag = request.session.get('bag', {})
         if not bag:
-            messages.error(
-                request, "There's nothing in your bag at the moment"
-            )
+            messages.error(request,
+                           "There's nothing in your bag at the moment")
             return redirect(reverse('products'))
 
         current_bag = bag_contents(request)
@@ -123,8 +123,9 @@ def checkout(request):
             order_form = OrderForm()
 
     if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. \
-            Did you forget to set it in your environment?')
+        messages.warning(request, ('Stripe public key is missing. '
+                                   'Did you forget to set it in '
+                                   'your environment?'))
 
     template = 'checkout/checkout.html'
     context = {
