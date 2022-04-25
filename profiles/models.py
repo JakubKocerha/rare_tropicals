099f9f433ff14575bbfe35e9a2da9b2a13675f2f
JakubKocerha <jakubbubble@gmail.com>
2022-04-25 15:35:29 +0000

    Debugging of admin review user item

--- profiles/models.py
+++ profiles/models.py
@@ -43,6 +43,8 @@ class UserProfile(models.Model):
         blank=True
     )
 
+    # def __str__(self):
+    #    return self.user.username
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
